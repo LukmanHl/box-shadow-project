346554fd06e1098cdbda763e59ce497d24dca090
Lukman Hammed <tec.lukman@gmail.com>
2025-08-23 22:13:03 +0100

    I created the boxShadow constannts and also created the functions that is will be creating box shadow, the functions that is also getting box shadow parameters and the function that genertates box shadow

--- script.js
+++ script.js
@@ -9,5 +9,49 @@ slidders.forEach((slidder) => slidder.addEventListener ("input", generateShadow)
 function generateShadow(){
   
   const shadowParametas = getShadowParametas();
+  const boxShadow = createBoxShadow(...shadowParametas);
+  applyShadow(elem, boxShadow);
 
+}
+
+function getShadowParametas(){
+
+  const hShadow = parseInt(document.getElementById("h-shadow").value);
+  const vShadow = parseInt(document.getElementById("v-shadow").value);
+  const blurRadius = parseInt(document.getElementById("blur-radius").value);
+  const spreadRadius = parseInt(document.getElementById("spread-radius").value);
+  const shadowColor = document.getElementById("shadow-color").value;
+  const shadowColorOpacity = document.getElementById("shadow-color-opacity").value;
+
+  const shadowInset = parseInt( document.getElementById("shadow-inset").checked).toFixed;
+
+  return[ hShadow,
+    vShadow,
+    blurRadius,
+    spreadRadius,
+    shadowColor,
+    shadowColorOpacity,
+  ]
+}
+
+
+function createBoxShadow(hShadow, vShadow, blurRadius, spreadRadius, color, opacity, inset ){
+
+  const shadow = inset ? "inset" : "";
+  const rgbaColor = hexToRgba(color, opacity);
+
+  return `${shadow} ${hShadow}px ${vShadow}px ${blurRadius}px ${spreadRadius}px ${rgbaColor}px `
+}
+
+function hexToRgba(color, opacity){
+  const r = parseInt(color.substr(1, 2), 16);
+  const g = parseInt(color.substr(3, 2), 16);
+  const b = parseInt(color.substr(5, 2), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
+
+
+applyShadow(element, boxShadow){
+   element.style.boxShadow = boxShadow;
 }
