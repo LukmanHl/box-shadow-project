cc1a3f1597702d40e90d0f8d38779891a68d6e8e
Lukman Hammed <tec.lukman@gmail.com>
2025-08-23 17:34:50 +0100

    i started adding the js scripts and event listeners

---
+++ script.js
@@ -0,0 +1,12 @@
+const elem = document.getElementById("element");
+const code = document.getElementById("code");
+
+const slidders = document.querySelectorAll(".slidders input");
+
+
+slidders.forEach((slidder) => slidder.addEventListener ("input", generateShadow));
+
+function generateShadow(){
+  
+
+}
